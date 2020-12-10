a0c512b66d33dbdb2f68d5844d3f2c44f7e30c42
ankitmandal24 <75557768+ankitmandal24@users.noreply.github.com>
2020-12-10 22:25:17 +0530

    first\

---
+++ numbers.py
@@ -0,0 +1,12 @@
+print(type(5)) #int use for whole number except division
+print(type(0.5)) #float use for decimal number and for division
+print(type(5/5))
+
+print(5**2) # here '**' means raise to the power of number
+
+print(6 % 4) # here % it means "modulo" gives remainder
+
+#maths function
+
+print(round(4.5)) # simply round off
+print(abs(-8)) # gives absolute value of any number
