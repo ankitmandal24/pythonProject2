f43c1390535ffd8c4b9afac11c278cf2c1e329fb
ankitmandal24 <75557768+ankitmandal24@users.noreply.github.com>
2020-12-14 22:57:46 +0530

    first\

---
+++ variable.py
@@ -0,0 +1,21 @@
+user_iq= 198
+print(user_iq)
+# rules
+# snake_case means use underscore instead of space
+#  start with lower case or underscore
+#  letters,numbers,underscore
+#  case sensitive
+#  don't overwrite keywords
+
+
+#  e.g. of last rule
+#print=190
+#print(print)
+PI=3.14
+PI=0
+print(PI)
+
+a,b,c=1,2,3
+print(a)
+print(b)
+print(c)
